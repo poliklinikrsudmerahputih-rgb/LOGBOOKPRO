672c2ae8663c03a9fc62340a47c0f5a0e7457d46
poliklinikrsudmerahputih-rgb <poliklinikrsudmerahputih@gmail.com>
2026-02-24 11:02:03 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const SB_URL = "https://orbpzmqquzlqztxvcswm.supabase.co";
-const SB_KEY = "sb_publishable_awj1FfT3WVkVWAxH6QLKmA_X1_K1JFe";
-
-// Gunakan nama supabaseClient agar sinkron dengan auth.js
-const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
